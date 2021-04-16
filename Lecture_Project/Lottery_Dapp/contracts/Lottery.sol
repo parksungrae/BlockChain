73cf8d760588154c8d6ace097bf29ee4783cc21d
parksungrae <qazwsxez0806@gmail.com>
2021-04-16 16:07:26 +0900

    2021_04_16_강의시청

--- Lecture_Project/Lottery_Dapp/contracts/Lottery.sol
+++ Lecture_Project/Lottery_Dapp/contracts/Lottery.sol
@@ -2,13 +2,57 @@ pragma solidity >=0.4.22 <0.9.0;
 
 
 contract Lottery {
+    struct BetInfo{
+        uint256 answerBlockNumber;
+        address payable bettor;
+        bytes challenges;
+    }
+
+    uint256 private _tail;
+    uint256 private _head;
+    mapping (uint256 => BetInfo) private _bets;
     address public owner;
 
-    constructor() public {
+    uint256 constant internal BLOCK_LIMIT = 256;
+    uint256 constant internal BET_BLOCK_INTERVAL = 3;
+    uint256 constant internal BET_AMOUNT = 5 * 10 ** 15;
+    uint256 private _pot;
+
+    constructor() public{
         owner = msg.sender;
     }
 
+    function bet(bytes memory challenges) public payable returns (bool result) {
+        
+    }
+    
     function getSomeValue() public pure returns (uint256 value) {
         return 5;
+    }
+
+    function getPot() public view returns(uint256 pot) {
+        return _pot;
+    }
+
+    function getBetInfo (uint256 index) public view returns (uint256 answerBlockNumber, address bettor, bytes memory challenges) {
+        BetInfo memory b = _bets[index];
+        answerBlockNumber = b.answerBlockNumber;
+        bettor = b.bettor;
+        challenges = b.challenges;
+    }
+    function pushBet(bytes memory challenges) public returns (bool) {
+        BetInfo memory b;
+        b.bettor = payable(msg.sender);
+        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL; 
+        b.challenges = challenges; 
+
+        _bets[_tail] = b;
+        _tail++;
+        return true;
+    }
+
+    function popBet(uint256 index) public returns (bool) {
+        delete _bets[index];
+        return true;
     }
 }
---
+++ Lecture_Project/Lottery_Dapp/test/lottery.test.js
@@ -0,0 +1,23 @@
+const Lottery = artifacts.require("Lottery");
+
+contract('Lottery', function([deployer, user1, user2]) {
+    beforeEach(async () => {
+        console.log('Before each')
+        lottery = await Lottery.new()
+    })
+
+    it('Basic test', async () => {
+        console.log('Basic test')
+        let owner = await lottery.owner();
+        let value = await lottery.getSomeValue();
+
+        console.log(`owner : ${owner}`);
+        console.log(`value : ${value}`);
+        assert.equal(value, 5)
+    }) 
+
+    it.only('getPot should return current pot', async () => {
+        let pot = await lottery.getPot();
+        assert.equal(pot, 0)
+    }) 
+});
